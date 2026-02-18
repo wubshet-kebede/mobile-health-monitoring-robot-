61f3020ffb29081eb80ec2a2ed5ba55577a00f07
Wube <wubshet2829@gmail.comgit config --global user.name Wubshet>
2026-02-18 23:22:51 +0300

    the nav2 config is completed

--- src/health_bot_description/launch/rsp.launch.py
+++ src/health_bot_description/launch/rsp.launch.py
@@ -30,7 +30,7 @@ def generate_launch_description():
     return LaunchDescription([
         DeclareLaunchArgument(
             'use_sim_time',
-            default_value='false',
+            default_value='true',
             description='Use simulation (Gazebo) clock if true'),
         node_robot_state_publisher
     ])
