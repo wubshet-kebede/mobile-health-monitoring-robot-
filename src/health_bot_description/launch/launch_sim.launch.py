2635fe3277275d148400ed5f0a9033729a34d911
Wube <wubshet2829@gmail.comgit config --global user.name Wubshet>
2026-02-21 22:00:16 +0300

    configuring custom SLAM toolbox for localization and mapping

--- src/health_bot_description/launch/launch_sim.launch.py
+++ src/health_bot_description/launch/launch_sim.launch.py
@@ -4,29 +4,41 @@ from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
-
 def generate_launch_description():
     pkg_name = 'health_bot_description'
     
-    # 1. Path Resolution
+    #  Path Resolution
     world_path = os.path.join(get_package_share_directory(pkg_name), 'worlds', 'health_hospital.world')
     bridge_params = os.path.join(get_package_share_directory(pkg_name), 'config', 'bridge_params.yaml')
-
-    # 2. Robot State Publisher (RSP)
     rsp = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([os.path.join(
             get_package_share_directory(pkg_name), 'launch', 'rsp.launch.py'
         )]), launch_arguments={'use_sim_time': 'true'}.items()
     )
+    slam = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory(pkg_name), 'launch', 'slam.launch.py'
+        )]), launch_arguments={'use_sim_time': 'true'}.items()
+    )
+    hospital_world_launch = IncludeLaunchDescription(
+    PythonLaunchDescriptionSource(
+        os.path.join(
+            get_package_share_directory('aws_robomaker_hospital_world'),
+            'launch',
+            'hospital.launch.py'   # or hospital_world.launch.py depending on repo
+        )
+    ),
+    launch_arguments={'use_sim_time': 'true'}.items()
+)
 
-    # 3. Gazebo Simulation
+    #  Gazebo Simulation
     gazebo = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([os.path.join(
             get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py'
         )]), launch_arguments={'gz_args': f'-r {world_path}'}.items()
     )
 
-    # 4. Spawn the Robot (Increased Z for safety)
+    #  Spawn the Robot 
     spawn_entity = Node(
         package='ros_gz_sim', 
         executable='create',
@@ -34,7 +46,7 @@ def generate_launch_description():
         output='screen'
     )
 
-    # 5. The ROS-GZ Bridge (The Missing Link)
+    #  The ROS-GZ Bridge 
     bridge = Node(
         package="ros_gz_bridge",
         executable="parameter_bridge",
@@ -44,11 +56,15 @@ def generate_launch_description():
         }],
         output="screen"
     )
+    
 
-    # 6. Return all actions to be executed
+    #  Return all actions to be executed
     return LaunchDescription([
+        hospital_world_launch,
         rsp,
         gazebo,
         spawn_entity,
-        bridge  # <--- Added to the list!
+        bridge,  
+        slam,
+        
     ])
