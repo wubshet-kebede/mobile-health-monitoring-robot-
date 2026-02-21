97e8e315f608fe2ebd7a7518381010761a5b4277
Wube <wubshet2829@gmail.comgit config --global user.name Wubshet>
2026-02-22 01:12:50 +0300

    hospital world configure half of it

--- src/health_hospital_world/download_models.sh
+++ src/health_hospital_world/download_models.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# List of models from the AWS Hospital World
+
 MODELS=("XRayMachine" "IVStand" "BloodPressureMonitor" "BPCart" "BMWCart" "CGMClassic" "StorageRack" "Chair" "InstrumentCart1" "Scrubs" "PatientWheelChair" "WhiteChipChair" "TrolleyBed" "SurgicalTrolley" "PotatoChipChair" "VisitorKidSit" "FemaleVisitorSit" "AdjTable" "MopCart3" "MaleVisitorSit" "Drawer" "OfficeChairBlack" "ElderLadyPatient" "ElderMalePatient" "InstrumentCart2" "MetalCabinet" "BedTable" "BedsideTable" "AnesthesiaMachine" "TrolleyBedPatient" "Shower" "SurgicalTrolleyMed" "StorageRackCovered" "KitchenSink" "Toilet" "VendingMachine" "ParkingTrolleyMin" "PatientFSit" "MaleVisitorOnPhone" "FemaleVisitor" "MalePatientBed" "StorageRackCoverOpen" "ParkingTrolleyMax")
 
 echo "Downloading hospital models for Harmonic..."
@@ -7,6 +7,19 @@ mkdir -p models
 
 for MODEL in "${MODELS[@]}"
 do
-    echo "Downloading $MODEL..."
-    gz fuel download -u https://fuel.gazebosim.org -m $MODEL -d models -v 4
+    echo "------------------------------------------"
+    echo "Downloading: $MODEL"
+    # Harmonic requires the full 1.0 API URL to avoid 'Missing Resource' errors
+    gz fuel download -v 4 -u "https://fuel.gazebosim.org"
 done
+
+# Copy from the hidden Gazebo cache to your local package
+echo "Moving models to local package..."
+# Note: In Harmonic, the cache path might be ~/.gz/fuel or ~/.ignition/fuel
+if [ -d "$HOME/.gz/fuel/models/openrobotics" ]; then
+    cp -rn $HOME/.gz/fuel/models/openrobotics/* ./models/
+elif [ -d "$HOME/.ignition/fuel/models/openrobotics" ]; then
+    cp -rn $HOME/.ignition/fuel/models/openrobotics/* ./models/
+fi
+
+echo "Done! Check your models folder now."
