caf578f8cb4ae6f701ca588c10754864a6d542cf
Wube <wubshet2829@gmail.comgit config --global user.name Wubshet>
2026-02-27 19:24:00 +0300

    bug fix

--- src/healthbot_ros2_control/launch/healthbot_control.launch.py
+++ src/healthbot_ros2_control/launch/healthbot_control.launch.py
@@ -1,15 +1,17 @@
+import os
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
-import os
 
 def generate_launch_description():
+    # Path to  modular YAML
     config = os.path.join(
         get_package_share_directory("healthbot_ros2_control"),
         "config",
         "healthbot_controllers.yaml"
     )
 
+    # Controller Manager node
     controller_manager = Node(
         package="controller_manager",
         executable="ros2_control_node",
@@ -17,6 +19,7 @@ def generate_launch_description():
         output="screen"
     )
 
+    # Spawner for Joint State Broadcaster
     spawner_jsb = Node(
         package="controller_manager",
         executable="spawner",
@@ -24,15 +27,25 @@ def generate_launch_description():
         output="screen"
     )
 
+    # Spawner for Diff Drive Controller
     spawner_diff = Node(
         package="controller_manager",
         executable="spawner",
-        arguments=["diff_drive_controller"],
+        arguments=["healthbot_controller"],
+        output="screen"
+    )
+
+    # Spawner for Simple Velocity Controller
+    spawner_vel = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["simple_velocity_controller"],
         output="screen"
     )
 
     return LaunchDescription([
         controller_manager,
         spawner_jsb,
-        spawner_diff
+        spawner_diff,
+        spawner_vel
     ])
